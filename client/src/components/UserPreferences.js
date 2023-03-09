78c105e201d7efa969e8d90aff405eed9c6f6322
Miladepour <epour.milad@gmail.com>
2023-03-09 08:34:38 +0000

    Basic setup of preferences form

---
+++ client/src/components/UserPreferences.js
@@ -0,0 +1,9 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import Form from "react-bootstrap/Form";
+
+export default function UserPreferences() {
+return(
+    <div>UserPreferences</div>
+);
+}
