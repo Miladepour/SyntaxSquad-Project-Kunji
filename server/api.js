b000b9e7672e5779ccf22c90f51783076bdbfa65
Cuneyt Turker <cuneytturker35@gmail.com>
2023-03-12 17:40:57 +0000

    add endpoint and some data

--- server/api.js
+++ server/api.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
-
+import { faker } from '@faker-js/faker';
 import logger from "./utils/logger";
+import { numeric } from "tar";
 
 const router = Router();
 
@@ -9,4 +10,44 @@ router.get("/", (_, res) => {
 	res.json({ message: "Hello, world!" });
 });
 
+router.get("/admin/users",(req,res) => {
+	const createFakeUsers = (arrayLength) => {
+		const arr = []
+		for(let i = 0; i<=arrayLength; i++){
+			arr.push({
+				id:faker.random.numeric(5),
+				name:faker.name.fullName(),
+				gender:faker.name.sex(),
+				dateOfBirth:faker.date.birthdate({ min: 18, max: 65, mode: 'age' }),
+				currentLocation:faker.address.cityName(),
+				pincode:faker.random.numeric(5),
+				phoneNumber:faker.phone.number('+44 91 ### ## ##'),
+				educationalQualification:faker.name.jobArea(),
+				dateOfRelease:faker.date.between('2020-01-01T00:00:00.000Z', '2023-01-01T00:00:00.000Z'),
+				caseStatus:faker.random.numeric(1)
+	
+			})
+		}
+		return arr
+	}
+	
+	res.json(createFakeUsers(5))
+	
+})
+
 export default router;
+// 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
