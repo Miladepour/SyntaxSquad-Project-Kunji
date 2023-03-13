912393f86281c72ad148332b498d66675491a66e
Haroon Ali <87202358+haroon-ali-dev@users.noreply.github.com>
2023-03-13 17:40:16 +0000

    add remaining fields

--- client/src/pages/admin/components/CreateNGO.js
+++ client/src/pages/admin/components/CreateNGO.js
@@ -7,7 +7,13 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
 const schema = yup.object({
-  service: yup.string().min(3).max(50).required().label("Service")
+  service: yup.string().min(3).max(50).required().label("Service"),
+  zone: yup.string().required("Please select zone.").label("Zone"),
+  organization: yup.string().min(3).max(100).required().label("Organization"),
+  address: yup.string().min(3).max(100).required().label("Address"),
+  contact: yup.string().min(3).max(50).required().label("Contact"),
+  website: yup.string().min(3).max(50).required().label("Website"),
+  email: yup.string().email().min(3).max(50).required().label("Email"),
 }).required();
 
 export default function CreateNGO({ createNGO, handleClose }) {
@@ -39,6 +45,121 @@ export default function CreateNGO({ createNGO, handleClose }) {
             />
             <Form.Control.Feedback type="invalid">
               {errors.service?.message}
+            </Form.Control.Feedback>
+          </Col>
+        </Row>
+      </Form.Group>
+
+      <Form.Group controlId="zone">
+        <Row>
+          <Col>
+            <Form.Label>Zone</Form.Label>
+          </Col>
+          <Col>
+            <Form.Select
+              aria-label="zone"
+              {...register("zone")}
+              isInvalid={errors?.zone}
+            >
+              <option value="">Select...</option>
+              <option value="North">North</option>
+              <option value="East">East</option>
+              <option value="South">South</option>
+              <option value="West">West</option>
+              <option value="Central">Central</option>
+            </Form.Select>
+            <Form.Control.Feedback type="invalid">
+              {errors.zone?.message}
+            </Form.Control.Feedback>
+          </Col>
+        </Row>
+      </Form.Group>
+
+      <Form.Group controlId="organization">
+        <Row>
+          <Col>
+            <Form.Label>Organization</Form.Label>
+          </Col>
+          <Col>
+            <Form.Control
+              type="text"
+              {...register("organization")}
+              isInvalid={errors.organization?.message}
+            />
+            <Form.Control.Feedback type="invalid">
+              {errors.organization?.message}
+            </Form.Control.Feedback>
+          </Col>
+        </Row>
+      </Form.Group>
+
+      <Form.Group controlId="address">
+        <Row>
+          <Col>
+            <Form.Label>Address</Form.Label>
+          </Col>
+          <Col>
+            <Form.Control
+              type="text"
+              {...register("address")}
+              isInvalid={errors.address?.message}
+            />
+            <Form.Control.Feedback type="invalid">
+              {errors.address?.message}
+            </Form.Control.Feedback>
+          </Col>
+        </Row>
+      </Form.Group>
+
+      <Form.Group controlId="contact">
+        <Row>
+          <Col>
+            <Form.Label>Contact</Form.Label>
+          </Col>
+          <Col>
+            <Form.Control
+              type="text"
+              {...register("contact")}
+              isInvalid={errors.contact?.message}
+            />
+            <Form.Control.Feedback type="invalid">
+              {errors.contact?.message}
+            </Form.Control.Feedback>
+          </Col>
+        </Row>
+      </Form.Group>
+
+      <Form.Group controlId="website">
+        <Row>
+          <Col>
+            <Form.Label>Website</Form.Label>
+          </Col>
+          <Col>
+            <Form.Control
+              type="text"
+              {...register("website")}
+              isInvalid={errors.website?.message}
+            />
+            <Form.Control.Feedback type="invalid">
+              {errors.website?.message}
+            </Form.Control.Feedback>
+          </Col>
+        </Row>
+      </Form.Group>
+
+      <Form.Group controlId="email">
+        <Row>
+          <Col>
+            <Form.Label>Email</Form.Label>
+          </Col>
+          <Col>
+            <Form.Control
+              type="email"
+              {...register("email")}
+              isInvalid={errors.email?.message}
+            />
+            <Form.Control.Feedback type="invalid">
+              {errors.email?.message}
             </Form.Control.Feedback>
           </Col>
         </Row>
