e0a8e78ddbe36dfcf99bd7257dc26b878f8e25f2
Haroon Ali <87202358+haroon-ali-dev@users.noreply.github.com>
2023-03-14 20:47:38 +0000

    change buttons to icons

--- client/src/pages/admin/NGOs.js
+++ client/src/pages/admin/NGOs.js
@@ -3,6 +3,7 @@ import fileData from "./data/ngos.json";
 import Table from "react-bootstrap/Table";
 import Modal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
+import Stack from 'react-bootstrap/Stack';
 import CreateNGO from "./components/CreateNGO";
 
 export default function NGOs() {
@@ -103,20 +104,53 @@ export default function NGOs() {
         <tbody>
           {ngos.map((ngo, i) => (
             <tr key={i}>
-              <td><ul>{ngo.services.map((service, i) => <li key={i}>{service}</li>)}</ul></td>
+              <td>
+                <Stack gap={3}>
+                  {ngo.services.map((service, i) => (
+                    <div key={i} className="border">{service}</div>
+                  ))}
+                </Stack>
+              </td>
               <td>{ngo.zone}</td>
               <td>{ngo.organization}</td>
               <td>{ngo.address}</td>
-              <td><ul>{ngo.contacts.map((contact, i) => <li key={i}>{contact}</li>)}</ul></td>
+              <td>
+                <Stack gap={3}>
+                  {ngo.contacts.map((contact, i) => (
+                    <div key={i} className="border">{contact}</div>
+                  ))}
+                </Stack>
+              </td>
               <td>{ngo.website}</td>
               <td>{ngo.email}</td>
               <td>
-                <Button variant="warning" onClick={() => update(ngo.id)}>
-                  Update
-                </Button>
-                <Button variant="danger" onClick={() => setShowDeleteModal([true, ngo.id])}>
-                  Delete
-                </Button>
+                <Stack direction="horizontal" gap={3}>
+                  <svg
+                    onClick={() => update(ngo.id)}
+                    fill="#fd7e14"
+                    viewBox="0 0 16 16"
+                    height="2em"
+                    width="2em"
+                    style={{ cursor: "pointer" }}
+                  >
+                    <path d="M15.502 1.94a.5.5 0 010 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 01.707 0l1.293 1.293zm-1.75 2.456l-2-2L4.939 9.21a.5.5 0 00-.121.196l-.805 2.414a.25.25 0 00.316.316l2.414-.805a.5.5 0 00.196-.12l6.813-6.814z" />
+                    <path
+                      fillRule="evenodd"
+                      d="M1 13.5A1.5 1.5 0 002.5 15h11a1.5 1.5 0 001.5-1.5v-6a.5.5 0 00-1 0v6a.5.5 0 01-.5.5h-11a.5.5 0 01-.5-.5v-11a.5.5 0 01.5-.5H9a.5.5 0 000-1H2.5A1.5 1.5 0 001 2.5v11z"
+                    />
+                  </svg>
+                  <svg
+                    onClick={() => setShowDeleteModal([true, ngo.id])}
+                    viewBox="0 0 1024 1024"
+                    fill="#dc3545"
+                    height="2em"
+                    width="2em"
+                    style={{ cursor: "pointer" }}
+                  >
+                    <path d="M864 256H736v-80c0-35.3-28.7-64-64-64H352c-35.3 0-64 28.7-64 64v80H160c-17.7 0-32 14.3-32 32v32c0 4.4 3.6 8 8 8h60.4l24.7 523c1.6 34.1 29.8 61 63.9 61h454c34.2 0 62.3-26.8 63.9-61l24.7-523H888c4.4 0 8-3.6 8-8v-32c0-17.7-14.3-32-32-32zm-200 0H360v-72h304v72z" />
+                  </svg>
+                </Stack>
+                
               </td>
             </tr>
           ))}
