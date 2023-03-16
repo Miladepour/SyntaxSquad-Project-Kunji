fd74d78279913f76e6ed44e957918c76110304ee
Miladepour <epour.milad@gmail.com>
2023-03-16 17:37:56 +0000

    Delete endpoint for NGO

--- server/routes/ngo.js
+++ server/routes/ngo.js
@@ -31,6 +31,22 @@ router.post("/", async (req, res) => {
   }
 });
 
+router.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const result = await db.query("DELETE FROM ngo WHERE id=$1 RETURNING *", [id]);
+    if (result.rowCount === 0) {
+      return res.status(404).json({ error: "NGO not found" });
+    }
+    res.json(`${id} has been deleted!`);
+  } catch (err) {
+    console.error("Failed to delete NGO from database:", err);
+    res.status(500).json({ error: "Failed to delete NGO" });
+  }
+});
+
+
 router.get("/", async (req, res) => {
   const ngos = await getNgo();
   res.json(ngos);
