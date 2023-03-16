b2247f3f21b88ad869e6f32aff24c1d3bdea13e9
Miladepour <epour.milad@gmail.com>
2023-03-16 16:56:12 +0000

    POST endpoint for NGO

--- server/routes/ngo.js
+++ server/routes/ngo.js
@@ -6,6 +6,30 @@ async function getNgo() {
   const { rows } = await db.query("SELECT * FROM ngo");
   return rows;
 }
+router.post("/", async (req, res) => {
+  const { body } = req;
+
+  try {
+    const result = await db.query(
+      "INSERT INTO ngo (service, zone, organization, address, contact, website, email, email_status, call_response) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING *",
+      [
+        body.service,
+        body.zone,
+        body.organization,
+        body.address,
+        JSON.stringify(body.contact),
+        body.website,
+        body.email,
+        body.email_status,
+        body.call_response,
+      ]
+    );
+    res.status(201).json(result.rows[0]);
+  } catch (err) {
+    console.error("Failed to insert NGO into database:", err);
+    res.status(500).json({ error: "Failed to create NGO" });
+  }
+});
 
 router.get("/", async (req, res) => {
   const ngos = await getNgo();
