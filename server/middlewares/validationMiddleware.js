6b585cdd5af5272954de40f143b9e56565b3afa1
Miladepour <epour.milad@gmail.com>
2023-03-18 14:41:40 +0000

    Add validation to PUT endpoint

--- server/middlewares/validationMiddleware.js
+++ server/middlewares/validationMiddleware.js
@@ -1,9 +1,9 @@
+import logger from "../utils/logger";
 const validate = (schema) => async (req, res, next) => {
   try {
     await schema.validate(req.body);
     next();
   } catch (err) {
-    console.error("Validation error:", err);
     res.status(400).json({ error: err.errors });
   }
 };
--- server/routes/ngo.js
+++ server/routes/ngo.js
@@ -29,12 +29,11 @@ router.post("/", validation(ngoSchema) ,async (req, res) => {
     );
     res.status(201).json(result.rows[0]);
   } catch (err) {
-    console.error("Failed to insert NGO into database:", err);
     res.status(500).json({ error: "Failed to create NGO" });
   }
 });
 
-router.put("/:id", async (req, res) => {
+router.put("/:id", validation(ngoSchema) , async (req, res) => {
   const { id } = req.params;
   const { body } = req;
 
@@ -42,15 +41,15 @@ router.put("/:id", async (req, res) => {
     const result = await db.query(
       "UPDATE ngo SET service=$1, zone=$2, organization=$3, address=$4, contact=$5, website=$6, email=$7, email_status=$8, call_response=$9 WHERE id=$10 RETURNING *",
       [
-        body.service || undefined,
-        body.zone || undefined,
-        body.organization || undefined,
-        body.address || undefined,
-        body.contact ? JSON.stringify(body.contact) : undefined,
-        body.website || undefined,
-        body.email || undefined,
-        body.email_status || undefined,
-        body.call_response || undefined,
+        body.service,
+        body.zone,
+        body.organization,
+        body.address,
+        JSON.stringify(body.contact),
+        body.website,
+        body.email,
+        body.email_status,
+        body.call_response,
         id,
       ]
     );
@@ -59,7 +58,6 @@ router.put("/:id", async (req, res) => {
     }
     res.json(`${id} has been updated!`);
   } catch (err) {
-    console.error("Failed to update NGO in database:", err);
     res.status(500).json({ error: "Failed to update NGO" });
   }
 });
@@ -75,7 +73,6 @@ router.delete("/:id", async (req, res) => {
     }
     res.json(`${id} has been deleted!`);
   } catch (err) {
-    console.error("Failed to delete NGO from database:", err);
     res.status(500).json({ error: "Failed to delete NGO" });
   }
 });
