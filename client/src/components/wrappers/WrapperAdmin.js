a51d38445f2ddbbb8ac5e850a30a880d826cacf7
Haroon Ali <87202358+haroon-ali-dev@users.noreply.github.com>
2023-03-20 15:27:29 +0000

    add navbar to admin section

--- client/src/components/wrappers/WrapperAdmin.js
+++ client/src/components/wrappers/WrapperAdmin.js
@@ -1,10 +1,23 @@
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
 
 export default function WrapperAdmin() {
   return (
     <div>
-      
+      <Navbar className="mb-3" bg="dark" variant="dark">
+        <Container>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link as={Link} to="/admin" >Users</Nav.Link>
+              <Nav.Link as={Link} to="/admin/ngos" >NGOs</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
       <Outlet />
     </div>
   );
