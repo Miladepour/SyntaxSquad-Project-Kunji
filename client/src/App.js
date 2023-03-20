9a3dd993be6a1cc6c84b3b8914adcc1679ac8f72
Haroon Ali <87202358+haroon-ali-dev@users.noreply.github.com>
2023-03-20 15:01:27 +0000

    change to nested routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,25 @@
 import { Route, Routes } from "react-router-dom";
-import Header from "./components/Header";
+import WrapperUser from "./components/wrappers/WrapperUser";
 import Home from "./pages/home/Home";
 import UserPreferences from "./pages/user-preferences/UserPreferences";
 import Result from "./pages/result/Result";
 import UserView from "./pages/admin/UserView";
 import NGOs from "./pages/admin/NGOs";
 import "bootstrap/dist/css/bootstrap.min.css";
+import WrapperAdmin from "./components/wrappers/WrapperAdmin";
 
 const App = () => (
 	<div>
-		<Header />
 		<Routes>
-			<Route path="/" element={<Home />} />
-			<Route path="/user-preferences" element={<UserPreferences />} />
-      <Route path="/result" element={<Result />} />
-	  <Route path="/admin/user-view" element={<UserView />} />
-      <Route path="/admin/ngos" element={<NGOs />} />
+			<Route path="/" element={<WrapperUser />}>
+				<Route path="/" element={<Home />} />
+				<Route path="/user-preferences" element={<UserPreferences />} />
+				<Route path="/result" element={<Result />} />
+			</Route>
+			<Route path="/admin" element={<WrapperAdmin />}>
+				<Route path="/admin" element={<UserView />} />
+				<Route path="/admin/ngos" element={<NGOs />} />
+			</Route>
 		</Routes>
 	</div>
 );
---
+++ client/src/components/wrappers/WrapperAdmin.js
@@ -0,0 +1,11 @@
+import { Outlet } from "react-router-dom";
+
+
+export default function WrapperAdmin() {
+  return (
+    <div>
+      
+      <Outlet />
+    </div>
+  );
+}
