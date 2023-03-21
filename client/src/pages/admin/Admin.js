eab77f958d5e08c3b2e904402e9013632d578816
Haroon Ali <87202358+haroon-ali-dev@users.noreply.github.com>
2023-03-21 16:26:48 +0000

    complete login page

--- client/src/pages/admin/Admin.js
+++ client/src/pages/admin/Admin.js
@@ -1,18 +1,24 @@
 import { useAuth0 } from '@auth0/auth0-react';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 
 export default function Admin() {
-  const { isAuthenticated, user, loginWithRedirect, logout } = useAuth0();
+  const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
 
   return(
     <div>
-      {isAuthenticated
-        ? <>
-          Hello {user.name}{' '}
-          <button onClick={() => logout({ returnTo: window.location.origin })}>
-            Log out
-          </button></>
-        : <button onClick={() => loginWithRedirect()}>Log in</button>
-      }
+      <Card className="mx-auto text-center" style={{ width: '18rem' }}>
+        <Card.Body>
+          <Card.Title>Kunji Admin Panel</Card.Title>
+          <Card.Text>
+            {!isAuthenticated ? "Please login to access." : "You are logged in."}
+          </Card.Text>
+          {!isAuthenticated
+            ? <Button variant="primary" onClick={() => loginWithRedirect()}>Login</Button>
+            : <Button variant="danger" onClick={() => logout({ returnTo: window.location.origin })}>Logout</Button>
+          }
+        </Card.Body>
+      </Card>
     </div>
   );
 }
