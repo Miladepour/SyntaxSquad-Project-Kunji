d861518b83a72b0c99cc2aad8d99a96df5319ec1
baharbazargan62 <105979952+baharbazargan62@users.noreply.github.com>
2023-03-22 11:57:52 +0000

    navar ckanges

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,7 +1,7 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
+
 
 function NavBar() {
   return (
@@ -12,18 +12,8 @@ function NavBar() {
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
+            <Nav.Link href="#link">Go to Form</Nav.Link>
+            <Nav.Link href="#link">Help-line Call  </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
