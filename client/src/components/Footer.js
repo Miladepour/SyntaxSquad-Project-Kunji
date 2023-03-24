c3543884a23fe7412f0ea17a995b076a140ea502
baharbazargan62 <105979952+baharbazargan62@users.noreply.github.com>
2023-03-24 12:38:28 +0000

    footer fixed
    Co-authored-by: Milad E-Pour <Miladepour@users.noreply.github.com>

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -2,11 +2,11 @@ import React from "react";
 import styles from "./Footer.module.css";
 function Footer() {
     return (
-      <>
-      <footer className={styles.siteFooter} >
+      <div id={styles["footer-container"]}>
+      <footer id={styles["site-footer"]} >
     <div className="text-center p-3" >Copyright reserved for Kunji</div>
   </footer>
-  </>
+  </div>
   );
 }
 export default Footer;
