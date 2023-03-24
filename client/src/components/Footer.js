fda9edeff6260a53922ceb54e7e86ca3f45a855e
baharbazargan62 <105979952+baharbazargan62@users.noreply.github.com>
2023-03-24 13:31:32 +0000

    footer  style changes

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,12 +1,10 @@
 import React from "react";
 import styles from "./Footer.module.css";
 function Footer() {
-    return (
-      <div id={styles["footer-container"]}>
-      <footer id={styles["site-footer"]} >
-    <div className="text-center p-3" >Copyright reserved for Kunji</div>
-  </footer>
-  </div>
-  );
+	return (
+		<footer id={styles["site-footer"]}>
+			<div className="text-center p-3">Copyright reserved for Kunji </div>
+		</footer>
+	);
 }
 export default Footer;
