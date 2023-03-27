ee20c5c4b049fa41b419afa5cd3d5c205825e8d9
Miladepour <epour.milad@gmail.com>
2023-03-27 11:04:18 +0100

    Add icon to hamburger style

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -4,33 +4,42 @@ import Navbar from "react-bootstrap/Navbar";
 import { Image } from "react-bootstrap";
 import logo from "../assest/kunji-logo2.svg";
 import styles from "./NavBar.module.css";
+import mobileMenuIcon from "../assest/menu-icon.svg";
 function NavBar() {
-
-  return (
-    <>
-    <Navbar className={styles.navbarHero} expand="lg">
-      <Container >
-        <Navbar.Brand href="/">
-          <Image src={logo} alt="Logo" height={70} />
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="justify-content-end flex-grow-1 pe-3" >
-            <Nav.Link href="/" className={styles.navbarLink}>Home</Nav.Link>
-            <Nav.Link href="/userform" className={styles.navbarLink}>Get help</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-          <div>
-          <button className={styles.buttonHelp} >Call Helpline</button>
-          </div>
-      </Container>
-    </Navbar>
-    <h5 className={styles.helpline}>
-  Help line (<a href="tel:18003134963" className={styles.helplineLink}>toll-free 1800-313-4963</a>)
-</h5>
-
-    </>
-  );
+	return (
+		<>
+			<Navbar className={styles.navbarHero} expand="lg">
+				<Container>
+					<Navbar.Brand href="/">
+						<Image src={logo} alt="Logo" height={70} />
+					</Navbar.Brand>
+					<Navbar.Toggle aria-controls="basic-navbar-nav" className="border-0">
+						<Image src={mobileMenuIcon} alt="Mobile Menu Icon" height={25} />
+					</Navbar.Toggle>
+					<Navbar.Collapse id="basic-navbar-nav">
+						<Nav className="justify-content-end flex-grow-1 pe-3">
+							<Nav.Link href="/" className={styles.navbarLink}>
+								Home
+							</Nav.Link>
+							<Nav.Link href="/userform" className={styles.navbarLink}>
+								Get help
+							</Nav.Link>
+						</Nav>
+					</Navbar.Collapse>
+					<div>
+						<button className={styles.buttonHelp}>Call Helpline</button>
+					</div>
+				</Container>
+			</Navbar>
+			<h5 className={styles.helpline}>
+				Help line (
+				<a href="tel:18003134963" className={styles.helplineLink}>
+					toll-free 1800-313-4963
+				</a>
+				)
+			</h5>
+		</>
+	);
 }
 
 export default NavBar;
