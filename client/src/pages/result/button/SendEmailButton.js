3ee596ba3d1ef369b2fe31a81060bdd284b26929
Miladepour <epour.milad@gmail.com>
2023-03-27 07:59:42 +0100

    Add isSending logic to email button.

--- client/src/pages/result/button/SendEmailButton.js
+++ client/src/pages/result/button/SendEmailButton.js
@@ -11,27 +11,33 @@ export default function SendEmailButton({ sendEmail }) {
   const [email, setEmail] = useState("");
   const [messageSent, setMessageSent] = useState(false);
   const [isValidEmail, setIsValidEmail] = useState(true);
+  const [isSending, setIsSending] = useState(false);
 
   function handleClose() {
     setShowModal(false);
     setEmail("");
     setMessageSent(false);
+    setIsSending(false);
   }
 
   function handleShow() {
     setShowModal(true);
     setEmail("");
     setMessageSent(false);
+    setIsSending(false);
   }
 
   async function handleSendEmail() {
     try {
+      setIsSending(true);
       await emailSchema.validate(email);
       setIsValidEmail(true);
-      sendEmail(email);
+      await sendEmail(email);
       setMessageSent(true);
     } catch (error) {
       setIsValidEmail(false);
+    } finally {
+      setIsSending(false);
     }
   }
 
@@ -49,7 +55,7 @@ export default function SendEmailButton({ sendEmail }) {
 
   return (
     <>
-      <button className="btn text-white m-2" type="button" style={{backgroundColor: "#004e87"}} onClick={handleShow}>
+      <button className="btn text-white m-2" type="button" style={{ backgroundColor: "#004e87" }} onClick={handleShow}>
         Send Email
       </button>
 
@@ -75,12 +81,12 @@ export default function SendEmailButton({ sendEmail }) {
           )}
         </Modal.Body>
         <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
+          <Button variant="secondary" onClick={handleClose} disabled={isSending}>
             Close
           </Button>
           {!messageSent && (
-            <Button variant="primary" onClick={handleSendEmail} disabled={!isValidEmail}>
-              Send Email
+            <Button variant="primary" onClick={handleSendEmail} disabled={!isValidEmail || isSending}>
+              {isSending ? "Sending..." : "Send Email"}
             </Button>
           )}
         </Modal.Footer>
