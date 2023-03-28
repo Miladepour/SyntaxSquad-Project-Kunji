cb232b3c82d9d135708dd0b7a5dd5551b2f9dd4a
Miladepour <epour.milad@gmail.com>
2023-03-28 16:12:14 +0100

    update images with main

--- client/src/components/Hero.js
+++ client/src/components/Hero.js
@@ -1,6 +1,14 @@
 import { Container, Row, Col, Image } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import styles from "./Hero.module.css";
+import imgOne from "../assets/kunji-1.jpg";
+import imgTwo from "../assets/kunji-2.jpg";
+import imgThree from "../assets/kunji-3.jpg";
+import imgFour from "../assets/kunji-4.jpg";
+import imgFive from "../assets/kunji-5.jpg";
+import imgSix from "../assets/kunji-6.jpg";
+import imgSeven from "../assets/kunji-7.jpg";
+import imgEight from "../assets/kunji-8.jpg";
 
 function Hero() {
   return (
@@ -12,7 +20,7 @@ function Hero() {
       <p>Welcome to Kunji, a charity organization dedicated to assisting ex-prisoners in finding the help they need to reintegrate into society. Our mission is to connect ex-prisoners with various NGOs, resources, and support systems to empower them in building a better future for themselves and their families.</p>
     </Col>
     <Col xs={12} md={6}className="order-md-1">
-      <Image src="https://i.ibb.co/j6f02Js/kunji-1.jpg" alt="Central jail in india" fluid className={styles.heroImages} />
+      <Image src={imgOne} alt="Central jail in india" fluid className={styles.heroImages} />
     </Col>
   </Row >
   <Row className="d-flex flex-row align-items-center my-5">
@@ -21,7 +29,7 @@ function Hero() {
       <p>A glimpse into the Indian prison system, where countless individuals seek a chance at a new beginning upon their release.</p>
     </Col>
     <Col  xs={12} md={6} className="order-md-2">
-      <Image src="https://i.ibb.co/HgTHb2j/kunji-2.jpg" alt="A man released from jail in india" fluid className={styles.heroImages} />
+      <Image src={imgTwo} alt="A man released from jail in india" fluid className={styles.heroImages} />
     </Col>
   </Row>
   <Row className="d-flex flex-row align-items-center my-5">
@@ -30,7 +38,7 @@ function Hero() {
       <p>A former inmate contemplates the many issues that need assistance, such as legal aid, healthcare, education, and more.</p>
     </Col>
     <Col xs={12} md={6} className="order-md-1">
-      <Image src="https://i.ibb.co/KFpXvbb/kunji-3.jpg" alt="A former inmate contemplates the many issues that need assistance" fluid className={styles.heroImages} />
+      <Image src={imgThree} alt="A former inmate contemplates the many issues that need assistance" fluid className={styles.heroImages} />
     </Col>
   </Row>
   <Row className="d-flex flex-row align-items-center my-5">
@@ -39,7 +47,7 @@ function Hero() {
       <p>The power of information: A passerby discovers Kunji’s life-changing advertisement on a bustling street, opening the door to a world of support and resources for ex-prisoners.</p>
     </Col>
     <Col xs={12} md={6} className="order-md-2">
-      <Image src="https://i.ibb.co/C9nJLPc/kunji-4.jpg" alt="life-changing advertisement on a bustling street" fluid className={styles.heroImages} />
+      <Image src={imgFour} alt="life-changing advertisement on a bustling street" fluid className={styles.heroImages} />
     </Col>
   </Row>
       <Row className="d-flex flex-row align-items-center my-5">
@@ -48,7 +56,7 @@ function Hero() {
           <p>Taking the first step: A former inmate reaches out to the Kunji helpline, seeking guidance and assistance in their journey towards a brighter future.</p>
         </Col>
         <Col xs={12} md={6} className="order-md-1">
-          <Image src="https://i.ibb.co/MGjCtmz/kunji-5.jpg" alt=" A former inmate reaches out to the Kunji helpline" fluid className={styles.heroImages} />
+          <Image src={imgFive} alt=" A former inmate reaches out to the Kunji helpline" fluid className={styles.heroImages} />
         </Col>
       </Row>
       <Row className="d-flex flex-row align-items-center my-5">
@@ -57,7 +65,7 @@ function Hero() {
           <p>A heartfelt conversation unfolds as the ex-prisoner explains the challenges they face to Kunji’s empathetic receptionist.</p>
         </Col>
         <Col xs={12} md={6} className="order-md-2">
-          <Image src="https://i.ibb.co/qypD9w8/kunji-6.jpg" alt="Kunji’s empathetic receptionist" fluid className={styles.heroImages} />
+          <Image src={imgSix} alt="Kunji’s empathetic receptionist" fluid className={styles.heroImages} />
         </Col>
       </Row>
       <Row className="d-flex flex-row align-items-center my-5">
@@ -66,7 +74,7 @@ function Hero() {
           <p>Avnish, our dedicated receptionist, helps the ex-prisoner navigate their situation, connecting them with the appropriate resources and support.</p>
         </Col>
         <Col xs={12} md={6} className="order-md-1">
-          <Image src="https://i.ibb.co/d7MbHHZ/kunji-7.jpg" alt="helps the ex-prisoner navigate their situation" fluid className={styles.heroImages} />
+          <Image src={imgSeven} alt="helps the ex-prisoner navigate their situation" fluid className={styles.heroImages} />
         </Col>
       </Row>
       <Row className="d-flex flex-row align-items-center my-5">
@@ -75,7 +83,7 @@ function Hero() {
           <p>With the right assistance, the ex-prisoner embarks on a journey of hope, healing, and happiness, empowered to create a brighter future.</p>
         </Col>
         <Col xs={12} md={6} className="order-md-2">
-          <Image src="https://i.ibb.co/hRmtwSv/kunji-8.jpg" alt=" the ex-prisoner found right help" fluid className={styles.heroImages} />
+          <Image src={imgEight} alt=" the ex-prisoner found right help" fluid className={styles.heroImages} />
         </Col>
       </Row>
     </Container>
