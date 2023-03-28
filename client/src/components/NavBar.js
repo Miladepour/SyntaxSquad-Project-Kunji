cb9bef7c69d41850c77d9e37aaf97e68809792ba
Miladepour <epour.milad@gmail.com>
2023-03-28 16:15:31 +0100

    Change the logo with white text

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -2,7 +2,7 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import { Image } from "react-bootstrap";
-import logo from "../assets/kunji-logo2.svg";
+import logo from "../assets/kunji-w-logo.svg";
 import styles from "./NavBar.module.css";
 import mobileMenuIcon from "../assets/menu-icon.svg";
 function NavBar() {
