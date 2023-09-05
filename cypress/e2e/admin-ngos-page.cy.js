8d9b5fb60a9d3eda29671d2a580d9ceb503568ed
Miladepour <epour.milad@gmail.com>
2023-09-05 18:21:11 +0100

    add form validation

--- cypress/e2e/admin-ngos-page.cy.js
+++ cypress/e2e/admin-ngos-page.cy.js
@@ -12,3 +12,23 @@ describe('Admin ngos', () => {
       cy.contains("Swaniawski-Roob").should("exist");
     })
   })
+
+  describe('Form validation', () => {
+    beforeEach(() => {
+      cy.task('seedDB');
+      cy.loginToAuth0(
+        Cypress.env('auth0_username'),
+        Cypress.env('auth0_password')
+      )
+      cy.visit('/admin/ngos');
+    });
+  
+    it('Required fields cannot be empty', () => {
+        cy.contains('Add New').click();
+        cy.get('[type=submit]').contains('Add').click();
+        cy.contains('Please select a service.').should('exist');
+        cy.contains('Please select zone.').should('exist');
+        cy.contains('Organization must be at least 3 characters').should('exist');
+        cy.contains('Phone Number must be at least 3 characters').should('exist');
+    })
+    })
