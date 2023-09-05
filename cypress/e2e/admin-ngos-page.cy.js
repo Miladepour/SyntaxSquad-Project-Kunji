7ae846070a5df72e95a3b45c7b958580a94abbb5
Miladepour <epour.milad@gmail.com>
2023-09-05 19:05:37 +0100

    add new NGO test

--- cypress/e2e/admin-ngos-page.cy.js
+++ cypress/e2e/admin-ngos-page.cy.js
@@ -1,3 +1,5 @@
+const { be } = require("date-fns/locale");
+
 describe('Admin ngos', () => {
     beforeEach(() => {
       cy.task('seedDB');
@@ -31,4 +33,26 @@ describe('Admin ngos', () => {
         cy.contains('Organization must be at least 3 characters').should('exist');
         cy.contains('Phone Number must be at least 3 characters').should('exist');
     })
+    })
+
+  describe('CRUD', () => {  
+    beforeEach(() => {
+        cy.task('seedDB');
+        cy.loginToAuth0(
+            Cypress.env('auth0_username'),
+            Cypress.env('auth0_password')
+        )
+        cy.visit('/admin/ngos');
+        });
+    it('Add NGO', () => {
+        cy.contains('Add New').click();
+        cy.get('#serviceDropdown').select('Legal Aid');
+        cy.get('#zone').select('North');
+        cy.get('#organization').type('Good People');
+        cy.get('#address').type('In the middle of nowhere');
+        cy.get('#contact').type('123456789');
+        cy.get('#website').type('www.goodpeople.com');
+        cy.get('#email_status').type('Yes');
+        cy.get('#call_response').type('Yes');
+        cy.get('[type=submit]').contains('Add').click();    })
     })
